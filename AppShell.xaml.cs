8cc621e78ed6880cb0edc9d2248aaf4e9c7749d7
stamenkovdmitriy <dervishs@gmail.com>
2023-06-24 20:30:24 +0300

    Добавьте файлы проекта.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Maui_Clock;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Maui_Clock;
+
+public partial class MainPage : ContentPage
+{
+    [Obsolete]
+    public MainPage()
+	{
+		InitializeComponent();
+
+        /* из кода создаем элемент
+        _timeLabel = new Label
+        {
+            HorizontalOptions = LayoutOptions.Center,
+            VerticalOptions = LayoutOptions.Center,
+            FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label))
+        };
+
+        Content = new StackLayout
+        {
+            Children = { _timeLabel }
+        };
+        */
+
+        Device.StartTimer(TimeSpan.FromSeconds(1), () =>
+        {
+            _timeLabel.Text = DateTime.Now.ToString("HH:mm:ss");
+            return true;
+        });
+    }
+
+	
+
+	
+}
+
